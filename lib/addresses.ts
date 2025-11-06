fd5226e746e31ce9dba570f8f24515cf26f23684
M4N4N22 <m4online1219@gmail.com>
2025-11-06 15:46:20 +0530

    bug fix

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,5 +1,5 @@
 export const VAULT_ADDRESSES: Record<number, string> = {
-    16661: process.env.NEXT_PUBLIC_VAULT_ADDRESS as string, // Aristotle Mainnet
+    16661: process.env.NEXT_PUBLIC_MAINNET_VAULT_ADDRESS as string, // Aristotle Mainnet
     16602: process.env.NEXT_PUBLIC_VAULT_ADDRESS as string, // Galileo Testnet
   };
   
